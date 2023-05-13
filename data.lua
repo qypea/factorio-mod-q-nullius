ea8b8949b5fbc5f3a6dce618c24646bd2fe74890
David Winn <q@qypea.com>
2023-05-13 12:43:23 -0700

    Move quantum boxes to storage category

--- data.lua
+++ data.lua
@@ -1,15 +1,14 @@
-
 -- Override item properties so they show up in nullius
 data.raw.item["QuantumResourceDistributionContainer"].order = "nullius-s"
-data.raw.item["QuantumResourceDistributionContainer"].subgroup = "equipment"
+data.raw.item["QuantumResourceDistributionContainer"].subgroup = "storage"
 data.raw.item["QuantumResourceDistributionCombinator"].order = "nullius-s"
-data.raw.item["QuantumResourceDistributionCombinator"].subgroup = "equipment"
+data.raw.item["QuantumResourceDistributionCombinator"].subgroup = "storage"
 
 -- Override recipes so they show up in nullius
 data.raw.recipe["QuantumResourceDistributionContainer"].order = "nullius-s"
-data.raw.recipe["QuantumResourceDistributionContainer"].subgroup = "equipment"
+data.raw.recipe["QuantumResourceDistributionContainer"].subgroup = "storage"
 data.raw.recipe["QuantumResourceDistributionCombinator"].order = "nullius-s"
-data.raw.recipe["QuantumResourceDistributionCombinator"].subgroup = "equipment"
+data.raw.recipe["QuantumResourceDistributionCombinator"].subgroup = "storage"
 
 -- Avoid problems with not having ingredients by the desired names
 data.raw.recipe["QuantumResourceDistributionContainer"].ingredients = {}
