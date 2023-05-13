8161850dae30066427c8a26b35d9e17fddd9f267
David Winn <q@qypea.com>
2023-05-13 12:43:38 -0700

    Add logic for dustbin

--- data.lua
+++ data.lua
@@ -3,12 +3,16 @@ data.raw.item["QuantumResourceDistributionContainer"].order = "nullius-s"
 data.raw.item["QuantumResourceDistributionContainer"].subgroup = "storage"
 data.raw.item["QuantumResourceDistributionCombinator"].order = "nullius-s"
 data.raw.item["QuantumResourceDistributionCombinator"].subgroup = "storage"
+data.raw.item["Dustbin"].order = "nullius-s"
+data.raw.item["Dustbin"].subgroup = "storage"
 
 -- Override recipes so they show up in nullius
 data.raw.recipe["QuantumResourceDistributionContainer"].order = "nullius-s"
 data.raw.recipe["QuantumResourceDistributionContainer"].subgroup = "storage"
 data.raw.recipe["QuantumResourceDistributionCombinator"].order = "nullius-s"
 data.raw.recipe["QuantumResourceDistributionCombinator"].subgroup = "storage"
+data.raw.recipe["Dustbin"].order = "nullius-s"
+data.raw.recipe["Dustbin"].subgroup = "storage"
 
 -- Avoid problems with not having ingredients by the desired names
 data.raw.recipe["QuantumResourceDistributionContainer"].ingredients = {}
