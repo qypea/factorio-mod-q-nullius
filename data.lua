e271ad52a7162b6719f3b92a06dad199194a3a90
David Winn <q@qypea.com>
2023-05-13 17:20:57 -0700

    Add flare stack support

--- data.lua
+++ data.lua
@@ -5,6 +5,8 @@ data.raw.item["QuantumResourceDistributionCombinator"].order = "nullius-s"
 data.raw.item["QuantumResourceDistributionCombinator"].subgroup = "storage"
 data.raw.item["Dustbin"].order = "nullius-s"
 data.raw.item["Dustbin"].subgroup = "storage"
+data.raw.item["vent-stack"].order = "nullius-s"
+data.raw.item["vent-stack"].subgroup = "air-treatment"
 
 -- Override recipes so they show up in nullius
 data.raw.recipe["QuantumResourceDistributionContainer"].order = "nullius-s"
@@ -13,7 +15,10 @@ data.raw.recipe["QuantumResourceDistributionCombinator"].order = "nullius-s"
 data.raw.recipe["QuantumResourceDistributionCombinator"].subgroup = "storage"
 data.raw.recipe["Dustbin"].order = "nullius-s"
 data.raw.recipe["Dustbin"].subgroup = "storage"
+data.raw.recipe["vent-stack"].order = "nullius-s"
+data.raw.recipe["vent-stack"].subgroup = "air-treatment"
 
 -- Avoid problems with not having ingredients by the desired names
 data.raw.recipe["QuantumResourceDistributionContainer"].ingredients = {}
 data.raw.recipe["QuantumResourceDistributionCombinator"].ingredients = {}
+data.raw.recipe["vent-stack"].ingredients = {}
