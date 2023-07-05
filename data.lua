e0060f57aa0018bfb7943cf8d3b7c4df61a9f565
David Winn <q@qypea.com>
2023-07-05 07:01:53 -0700

    Add unipipe support

--- data.lua
+++ data.lua
@@ -8,6 +8,11 @@ data.raw.item["Dustbin"].subgroup = "storage"
 data.raw.item["vent-stack"].order = "nullius-s"
 data.raw.item["vent-stack"].subgroup = "air-treatment"
 
+data.raw.item["zy-unipipe-fill"].order = "nullius-s"
+data.raw.item["zy-unipipe-fill"].subgroup = "air-treatment"
+data.raw.item["zy-unipipe-extract"].order = "nullius-s"
+data.raw.item["zy-unipipe-extract"].subgroup = "air-treatment"
+
 -- Override recipes so they show up in nullius
 data.raw.recipe["QuantumResourceDistributionContainer"].order = "nullius-s"
 data.raw.recipe["QuantumResourceDistributionContainer"].subgroup = "storage"
@@ -17,12 +22,21 @@ data.raw.recipe["Dustbin"].order = "nullius-s"
 data.raw.recipe["Dustbin"].subgroup = "storage"
 data.raw.recipe["vent-stack"].order = "nullius-s"
 data.raw.recipe["vent-stack"].subgroup = "air-treatment"
+data.raw.recipe["zy-unipipe-fill"].order = "nullius-s"
+data.raw.recipe["zy-unipipe-fill"].subgroup = "air-treatment"
+data.raw.recipe["zy-unipipe-extract"].order = "nullius-s"
+data.raw.recipe["zy-unipipe-extract"].subgroup = "air-treatment"
 
 -- Avoid problems with not having ingredients by the desired names
 data.raw.recipe["QuantumResourceDistributionContainer"].ingredients = {}
 data.raw.recipe["QuantumResourceDistributionCombinator"].ingredients = {}
 data.raw.recipe["vent-stack"].ingredients = {}
+data.raw.recipe["zy-unipipe-fill"].ingredients = {}
+data.raw.recipe["zy-unipipe-extract"].ingredients = {}
 
--- Enable vent stack recipe without research we don't have in nullius
--- Note: In order for vent-stack to work with nullius you'll need to patch it to return noicons when no icon is present for an item
+-- Enable recipe without research we don't have in nullius
 data.raw.recipe["vent-stack"].enabled = true
+data.raw.recipe["zy-unipipe-fill"].enabled = true
+data.raw.recipe["zy-unipipe-extract"].enabled = true
+
+-- Note: In order for vent-stack to work with nullius you'll need to patch it to return noicons when no icon is present for an item
